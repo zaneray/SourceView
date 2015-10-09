e14e71f74387884211fd6f9d514f12c8581ca132
Flori Seeger <florian@zaneray.com>
2015-10-09 09:58:41 -0600

    formatting

--- source-view.js
+++ source-view.js
@@ -1,39 +1,35 @@
-<script type="text/javascript">
+(function($) {
+	$.fn.SourceCodeView = function(element){
+		this.each(function(){
+		
+			var button = $('<button>');
+			var container = $('<div>');
 
-	(function($) {
-		$.fn.SourceCodeView = function(element){
-			this.each(function(){
-			
-				var button = $('<button>');
-				var container = $('<div>');
+			button.html('View Source Code');
+			button.addClass('btn');
 
-				button.html('View Source Code');
-				button.addClass('btn');
+			container.html("<xmp>" + $(this).html() + "</xmp>");
+			container.addClass('sourceviewer');
 
-				container.html("<xmp>" + $(this).html() + "</xmp>");
-				container.addClass('sourceviewer');
+			$(this).after(button);
+			button.after(container);
 
-				$(this).after(button);
-				button.after(container);
+			button.on('click', function(){
+				if ($(this).hasClass('showing')){
+					$(this).removeClass('showing');
+					$(this).html('View Source Code');
+				} else {
+					$(this).addClass('showing');
+					$(this).html('Hide Source Code');
+				}
 
-				button.on('click', function(){
-					if ($(this).hasClass('showing')){
-						$(this).removeClass('showing');
-						$(this).html('View Source Code');
-					} else {
-						$(this).addClass('showing');
-						$(this).html('Hide Source Code');
-					}
-
-					$(this).next().slideToggle( "slow", function() {
-	    			// nothing for now
-	  			});
-				});
-
+				$(this).next().slideToggle( "slow", function() {
+    			// nothing for now
+  			});
 			});
 
-		}
-		
-	})(jQuery);
-	// 
-</script>
+		});
+
+	}
+	
+})(jQuery);
